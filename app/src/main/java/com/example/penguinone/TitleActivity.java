ec1771d7e0a6c5270c660441b7d96d73284c08b2
Michael Zhao <michaelzhao314@gmail.com>
2020-08-12 15:23:56 -0500

    Create title layout and 1 routing

--- app/src/main/java/com/example/penguinone/TitleActivity.java
+++ app/src/main/java/com/example/penguinone/TitleActivity.java
@@ -2,7 +2,9 @@ package com.example.penguinone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+    }
+
+    public void next(View view) {
+        Intent intent = new Intent(this, TransitionActivity.class);
+        startActivity(intent);
     }
 }
