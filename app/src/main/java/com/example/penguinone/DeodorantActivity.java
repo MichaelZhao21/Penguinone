2cfb719b42c6c31efbc16a014fac32d4a547d0c5
Michael Zhao <michaelzhao314@gmail.com>
2020-10-19 14:58:11 -0500

    Added after quiz comment

--- app/src/main/java/com/example/penguinone/DeodorantActivity.java
+++ app/src/main/java/com/example/penguinone/DeodorantActivity.java
@@ -13,5 +13,4 @@ public class DeodorantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deodorant);
     }
-
 }
--- app/src/main/java/com/example/penguinone/QuizAfterActivity.java
+++ app/src/main/java/com/example/penguinone/QuizAfterActivity.java
@@ -15,9 +15,26 @@ public class QuizAfterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_after);
-        int score = getIntent().getExtras().getInt("score");
-        TextView scoreElement = (TextView) findViewById(R.id.score);
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras == null) return;
+
+        int score = extras.getInt("score");
+
+        TextView scoreElement = findViewById(R.id.score);
         scoreElement.setText(String.format(Locale.US, "%d%%", score));
+
+        TextView comment = findViewById(R.id.quizComment);
+        if (score >= 80) {
+            comment.setText(R.string.quiz80);
+        }
+        else if (score >= 50) {
+            comment.setText(R.string.quiz50);
+        }
+        else {
+            comment.setText(R.string.quiz0);
+        }
     }
 
     public void home(View view) {
