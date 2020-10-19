a65833af2d2c9309a3ff258fb2e58de1d157c275
Michael Zhao <michaelzhao314@gmail.com>
2020-10-19 15:25:14 -0500

    Added external links

--- app/src/main/java/com/example/penguinone/DeodorantActivity.java
+++ app/src/main/java/com/example/penguinone/DeodorantActivity.java
@@ -3,8 +3,10 @@ package com.example.penguinone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class DeodorantActivity extends AppCompatActivity {
 
@@ -12,5 +14,20 @@ public class DeodorantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deodorant);
+    }
+
+    public void switchText(View view) {
+        TextView words = findViewById(R.id.deoText);
+        if (words.getText().toString().equals(getResources().getString(R.string.deodorant1))) {
+            words.setText(R.string.deodorant2);
+        }
+        else {
+            words.setText(R.string.deodorant1);
+        }
+    }
+
+    public void deoAlt(View view) {
+        Intent openPage = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.nativecos.com/products/coconut-vanilla-deo-womens?gclid=CjwKCAjwz6_8BRBkEiwA3p02Vcz99NqE1Hym_Ivvno-yMjuETOEiSIT_SKyg2pQRgWHcEFeTWmZ7fhoC1QUQAvD_BwE"));
+        startActivity(openPage);
     }
 }
--- app/src/main/java/com/example/penguinone/DishSoapActivity.java
+++ app/src/main/java/com/example/penguinone/DishSoapActivity.java
@@ -2,7 +2,11 @@ package com.example.penguinone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class DishSoapActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class DishSoapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dish_soap);
+    }
+
+    public void switchText(View view) {
+        TextView words = findViewById(R.id.soapText);
+        if (words.getText().toString().equals(getResources().getString(R.string.soap1))) {
+            words.setText(R.string.soap2);
+        }
+        else {
+            words.setText(R.string.soap1);
+        }
+    }
+
+    public void soapAlt(View view) {
+        Intent openPage = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.amazon.com/ECOS-Dishmate-Dish-Liquid-Clear/dp/B004YIXB3C?ots=1&tag=epicurious09-20&linkCode=w50"));
+        startActivity(openPage);
     }
 }
